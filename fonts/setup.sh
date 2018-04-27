2b475cce36a4305957cb97f58065d5bb7486280e
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-04-27 18:24:28 -0400

    Added powerline fonts.

---
+++ fonts/setup.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+info "Configuraing fonts..."
+
+git submodule update --init
+cp powerline-fonts/Meslo/* /Library/Fonts/
+cp powerline-fonts/Menlo/* /Library/Fonts/
+
+success "Finished configuring fonts."
