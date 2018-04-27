2a6ec3aa1df699ca93cbedc6c786780921ba2aa9
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-04-27 17:23:31 -0400

    Add drew-vimrc subrepo to vim configuration.

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,9 +10,8 @@ DESTINATION="$(realpath ~)"
 
 info "Setting up Vim..."
 
-find . -name ".vim*" | while read fn; do
-    fn=$(basename $fn)
-    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
-done
+git submodule update --init
+
+./drew-vimrc/setup_vimrc.sh
 
 success "Finished setting up Vim."
