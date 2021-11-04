db099b53608ef298eecb84f0e01d5727f781194e
Drew Gottlieb <gottlieb.drew@gmail.com>
2021-11-04 00:08:30 -0700

    Add TopNotch.app.

---
+++ packages/casks/topnotch.rb
@@ -0,0 +1,11 @@
+cask "topnotch" do
+  version "latest"
+  sha256 :no_check
+
+  url "https://updates.topnotch.app/TopNotch-latest.zip"
+  name "Shearwater Cloud"
+  desc ""
+  homepage "https://topnotch.app"
+
+  app "TopNotch.app"
+end
