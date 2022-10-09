72620088a1eeaa290b86db723d127c2d09bd761e
Drew Gottlieb <gottlieb.drew@gmail.com>
2022-10-08 18:55:40 -0700

    Update Shearwater Cloud to 2.9.4

--- packages/casks/shearwater-cloud.rb
+++ packages/casks/shearwater-cloud.rb
@@ -1,8 +1,8 @@
 cask "shearwater-cloud" do
-  version "2.8.2"
+  version "2.9.4"
   sha256 :no_check
 
-  url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_2.8.2.dmg"
+  url "https://www.shearwater.com/wp-content/downloads/ShearwaterCloudInstaller_2.9.4.dmg"
   name "Shearwater Cloud"
   desc ""
   homepage "https://www.shearwater.com/cloud-downloads/"
