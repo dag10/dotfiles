41ab073065253550669d1ac942270bf094c9fd93
Drew Gottlieb <gottlieb.drew@gmail.com>
2024-06-20 23:39:25 -0700

    Overhaul tmux profile.
    Now we use the tmux plugin manager, and install the power plugin that renders a pretty tmux status bar.

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#!/bin/bash
 
 DIR=$(dirname "$0")
 cd "$DIR"
@@ -16,5 +16,13 @@ find . -name ".tmux*" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
+
+info "Downloading tmux plugin manager..."
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+info "Installing tmux plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
 
 success "Finished configuring tmux."
