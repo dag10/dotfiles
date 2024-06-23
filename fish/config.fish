e80c0fd56c56f3101370eb0c0c59e7f9ac804cd5
Drew Gottlieb <gottlieb.drew@gmail.com>
2024-06-23 15:02:09 -0700

    Better tmuxa/tmuxn commands.
    Now I can use `tmuxa` without any args to interactively browse
    all current tmux sessions (if >1) or just attach the sole existing
    session.
    
    And if I accidentally type `tmuxn` without a name by muscle memory,
    it'll still open a new tmux session. Even though I could've just typed
    `tmux`.
    
    Also adds TMUX iTerm integration, but that's only useful if I
    use `tmux -CC` which I don't see myself doing.

--- fish/config.fish
+++ fish/config.fish
@@ -6,10 +6,30 @@ set -g theme_show_exit_status yes
 # Theme settings
 set -g theme_color_scheme base16
 
-# Aliases
-#alias composer "php ~/software/composer/composer.phar"
-alias tmuxn "tmux -2 new-session -s"
-alias tmuxa "tmux -2 attach-session -t"
+# Smart functions for createing and attaching tmux sessions.
+function tmuxl
+  tmux -2 attach\; choose-tree -s
+end
+function tmuxn
+  if test (count $argv) -gt 0
+    tmux -2 new-session -s $argv[1]
+  else
+    tmux -2 new-session
+  end
+end
+function tmuxa
+  if set -q TMUX
+    tmux -2 switch -t $argv[1]
+  else if test (count $argv) -gt 0
+    tmux -2 attach-session -t $argv[1]
+  else if [ (count (tmux ls 2>/dev/null)) -gt 1 ]
+    tmux -2 attach\; choose-tree -s
+  else
+    tmux -2 attach
+  end
+end
+
+# Other aliases
 alias rg "npm run gulp"
 alias rt "npm run tslint"
 alias rc "npm run gulp css"
@@ -22,6 +42,7 @@ fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 
 # Integrate iTerm2
+set -g ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX 1
 source ~/.config/fish/iterm2_shell_integration.fish
 
 # Integrate fzf
